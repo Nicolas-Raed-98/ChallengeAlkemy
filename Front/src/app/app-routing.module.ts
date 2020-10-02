77f5a71bb4808f8cad14feabe4117a26993e5616
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-10-02 03:58:57 -0300

    Rutas y componentes Admin y Alumno

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
+import { LoginAlumnoComponent } from './components/login-alumno/login-alumno.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: 'Inicio' },
+  { path: "Inicio", component: InicioComponent, pathMatch: "full" },
+  { path: "loginAdmin", component: LoginAdminComponent, pathMatch: "full" },
+  { path: "loginAlumno", component: LoginAlumnoComponent, pathMatch: "full" }
 
-
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
