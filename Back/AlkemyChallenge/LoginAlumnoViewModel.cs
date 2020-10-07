84cd745999d3d97edfce891bdcc01e611922045f
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-10-07 04:31:37 -0300

    Completar Entidades

---
+++ Back/AlkemyChallenge/LoginAlumnoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlkemyChallenge
+{
+    class LoginAlumnoViewModel
+    {
+        public AlumnoViewModel alumno { get; set; }
+    }
+}
