000bd6ad8ef46d72a7c85768d47e4b850cfb7b6a
Nicolas-Raed-98 <61191717+Nicolas-Raed-98@users.noreply.github.com>
2020-10-07 03:33:53 -0300

    Entidades

---
+++ Back/AlkemyChallenge/ProfesorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlkemyChallenge
+{
+    class ProfesorViewModel
+    {
+        public int IdProfesor { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string dni { get; set; }
+        public string estado { get; set; }
+    }
+}
